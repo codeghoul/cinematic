8e034db5ba79c17c83606d72164d1a3b7be7794c
Jayesh Patel <jayesh.patel.2197@gmail.com>
2019-08-18 00:37:01 +0530

    Initial Project Add

---
+++ src/main/java/me/jysh/cinematic/CinematicApplication.java
@@ -0,0 +1,13 @@
+package me.jysh.cinematic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jysh/cinematic/CinematicApplicationTests.java
@@ -0,0 +1,16 @@
+package me.jysh.cinematic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinematicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
